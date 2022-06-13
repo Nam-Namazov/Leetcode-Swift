335a1fcf9104d8fa0410204fda9a7fa326a4dd87
Namik <namazov306@gmail.com>
2022-06-13 13:01:06 +0300

    solve problem

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,58 @@
 import Foundation
+// MARK: Binary Tree Inorder Traversal
+
+// Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+// Constraints:
+//The number of nodes in the tree is in the range [0, 100].
+//-100 <= Node.val <= 100
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func inorderTraversal(_ root: TreeNode?) -> [Int] {
+        var result = [Int]()
+        var stack = [TreeNode]()
+        var node = root
+        
+        while node != nil || !stack.isEmpty {
+            if let currentNode = node {
+                stack.append(currentNode)
+                node = currentNode.left
+            } else {
+                let previousNode = stack.removeLast()
+                result.append(previousNode.val)
+                node = previousNode.right
+            }
+        }
+        return result
+    }
+}
