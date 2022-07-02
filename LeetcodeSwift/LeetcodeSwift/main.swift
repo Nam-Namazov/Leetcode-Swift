777e17b55a8d80caccbc67321716fcf0cf4cc31d
Namik <namazov306@gmail.com>
2022-07-02 11:45:08 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,37 @@
 import Foundation
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init() { self.val = 0; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+ *     public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+ *         self.val = val
+ *         self.left = left
+ *         self.right = right
+ *     }
+ * }
+ */
+
+class Solution {
+    func sortedArrayToBST(_ nums: [Int]) -> TreeNode? {
+        if nums.isEmpty { return nil }
+        return build(nums, 0, nums.count - 1)
+    }
+    
+    private func build( _ nums: [Int],
+                       _ start: Int,
+                       _ end: Int) -> TreeNode? {
+        if start > end { return nil }
+        
+        let mid = start + (end - start) / 2
+        let midValue = nums[mid]
+
+        let rootNode = TreeNode(midValue)
+        rootNode.left = build(nums, start, mid - 1)
+        rootNode.right = build(nums, mid + 1, end)
+        return rootNode
+    }
+}
