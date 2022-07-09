2c1af33517d8d2cb4a5ec4575f516aed5ff718d4
Namik <namazov306@gmail.com>
2022-07-09 23:54:04 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,13 @@
 import Foundation
+
+func singleNumber(_ nums: [Int]) -> Int {
+    
+    var result = 0
+    
+    for i in nums {
+        result ^= i
+    }
+    return result
+}
+
+print(singleNumber([2, 2, 1]))
