9d490bf7f5f131f17d2aa08db57da305265b4f0a
Namik <namazov306@gmail.com>
2022-07-10 20:34:08 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,14 @@
 import Foundation
+// Given an array nums containing n distinct numbers in the range [0, n], return the only number in the range that is missing from the array.
+// [3, 0, 1]
+
+func missingNumber(_ nums: [Int]) -> Int {
+    var sumOfNumbers = (nums.count * (nums.count + 1)) / 2
+    
+    for num in nums {
+        sumOfNumbers -= num
+    }
+    return sumOfNumbers
+}
+
+print(missingNumber([3, 0, 1]))
