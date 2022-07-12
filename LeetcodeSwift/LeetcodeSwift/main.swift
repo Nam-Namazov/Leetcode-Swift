d172e3c2bafa77f9b0324e269f18b2d929947367
Namik <namazov306@gmail.com>
2022-07-13 01:58:00 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,29 @@
 import Foundation
+
+//Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func isSymmetric(_ root: TreeNode?) -> Bool {
+        func isMirror(left: TreeNode?, right: TreeNode?) -> Bool {
+            if left == nil && right == nil { return true }
+            guard let left = left,
+                  let right = right else { return false }
+            return left.val == right.val &&
+            isMirror(left: left.left,right: right.right) &&
+            isMirror(left: left.right, right: right.left)
+        }
+        return isMirror(left: root, right: root)
+    }
+}
