67af8b8faecaee925ddf7bfabcdea01a813012c9
Namik <namazov306@gmail.com>
2022-07-13 02:00:53 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,16 @@
 import Foundation
+class Solution {
+    func merge(_ nums1: inout [Int], _ m: Int, _ nums2: [Int], _ n: Int) {
+        var i = m - 1, j = n - 1
+        
+        while i >= 0 || j >= 0 {
+            if j < 0 || (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[i + j + 1] = nums1[i]
+                i -= 1
+            } else {
+                nums1[i + j + 1] = nums2[j]
+                j -= 1
+            }
+        }
+    }
+}
