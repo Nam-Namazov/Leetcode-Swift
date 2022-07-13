1a2efbc95a38147f01ea2712dad19f2d63e70e1c
Namik <namazov306@gmail.com>
2022-07-14 02:47:50 +0300

    make another solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -5,27 +5,20 @@ import Foundation
  You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 
  Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
-        [7, 1, 5, 3, 6, 4]
  */
 
-class Solution {
-    func maxProfit(_ prices: [Int]) -> Int {
-        
-        if prices.isEmpty {
-            return 0
+func maxProfit(_ prices: [Int]) -> Int {
+    var minPrice = Int.max
+    var maxProfit = 0
+    
+    for i in 0..<prices.count {
+        if prices[i] < minPrice {
+            minPrice = prices[i]
+        } else if prices[i] - minPrice > maxProfit {
+            maxProfit = prices[i] - minPrice
         }
-        
-        var min = prices[0]
-        var profit = 0
-        
-        for i in prices {
-            if i < min {
-                min = i
-            } else if ((i - min) > profit) {
-                profit = i - min
-            }
-        }
-        
-        return profit
     }
+    return maxProfit
 }
+
+print(maxProfit([7, 9, 1, 2]))
