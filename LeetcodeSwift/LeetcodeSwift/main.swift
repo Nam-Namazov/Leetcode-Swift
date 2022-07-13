c4488ea6ab5dd61bfa10c2d52d5f7f25c798ad35
Namik <namazov306@gmail.com>
2022-07-13 22:33:45 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,31 @@
 import Foundation
+// MARK: - Best Time to Buy and Sell Stock
+/* You are given an array prices where prices[i] is the price of a given stock on the ith day.
+ 
+ You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+ Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+        [7, 1, 5, 3, 6, 4]
+ */
+
+class Solution {
+    func maxProfit(_ prices: [Int]) -> Int {
+        
+        if prices.isEmpty {
+            return 0
+        }
+        
+        var min = prices[0]
+        var profit = 0
+        
+        for i in prices {
+            if i < min {
+                min = i
+            } else if ((i - min) > profit) {
+                profit = i - min
+            }
+        }
+        
+        return profit
+    }
+}
