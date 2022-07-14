25a1c4282ebb94ff32d490998a25786d3d518735
Namik <namazov306@gmail.com>
2022-07-15 02:06:00 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,20 @@
 import Foundation
+// MARK: - First Unique Character in a String
+// leelcode -> 0 else -> -1
+class Solution {
+    func firstUniqChar(_ s: String) -> Int {
+        let s = Array(s)
+        var hashTable: [Character: Int] = [:]
+        
+        for i in 0..<s.count {
+            hashTable[s[i]] = hashTable[s[i]] == nil ? i : -1
+        }
+        
+        for i in 0..<s.count {
+            if hashTable[s[i]] != -1 {
+                return i
+            }
+        }
+        return -1
+    }
+}
