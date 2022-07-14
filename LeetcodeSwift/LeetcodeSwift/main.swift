e1b9ffc8f4cc7a465f85eba80585429b7c8c1341
Namik <namazov306@gmail.com>
2022-07-14 23:34:33 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,23 @@
 import Foundation
+// MARK: - Delete node in a linked list
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public var val: Int
+ *     public var next: ListNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.next = nil
+ *     }
+ * }
+ *
+ * Write a function to delete a node in a singly-linked list. You will not be given access to the head of the list, instead you will be given access to the node to be deleted directly.
+    It is guaranteed that the node to be deleted is not a tail node in the list.
+ */
+
+class Solution {
+    func deleteNode(_ node: ListNode?) {
+        node!.val = node!.next!.val
+        node!.next = node!.next!.next
+    }
+}
