fb6ad3be1542985637516c99ea3733f83c7258ba
Namik <namazov306@gmail.com>
2022-07-14 23:19:51 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,20 @@
 import Foundation
+
+//MARK: - Move Zeroes
+// [0,1,0,3,12] -> [1,3,12,0,0]
+//
+
+class Solution {
+    func moveZeroes(_ nums: inout [Int]) {
+        var counter = 0
+        
+        for i in 0..<nums.count {
+            if nums[i] != 0 {
+                let temp = nums[counter]
+                nums[counter] = nums[i]
+                nums[i] = temp
+                counter += 1
+            }
+        }
+    }
+}
