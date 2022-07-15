6e804e45d9d0ad1ccd72b906eb95b21e2ae2cf57
Namik <namazov306@gmail.com>
2022-07-15 14:51:44 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,39 @@
 import Foundation
+// MARK: - Diameter of binary tree
+
+//Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+
+class Solution {
+    var diameter = 0
+    
+    func diameterOfBinaryTree(_ root: TreeNode?) -> Int {
+        maxDepth(root)
+        
+        return diameter
+    }
+    
+    fileprivate func maxDepth(_ node: TreeNode?) -> Int {
+        guard let node = node else {
+            return 0
+        }
+        
+        let (left, right) = (maxDepth(node.left), maxDepth(node.right))
+        
+        diameter = max(diameter, left + right)
+        
+        return max(left, right) + 1
+    }
+}
