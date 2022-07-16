ebab28e5a2796287ac842dd0f081763a600f781d
Namik <namazov306@gmail.com>
2022-07-16 03:59:36 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,23 @@
 import Foundation
+// MARK: - Valid Parantheses
+
+class Solution {
+    let dictionary: [Character: Character] = [")": "(", "}": "{", "]": "["]
+    
+    func isValid(_ s: String) -> Bool {
+        var stack = [Character]()
+        
+        for char in s {
+            if char == "(" || char == "{" || char == "[" {
+                stack.append(char)
+            } else {
+                guard let lastChar = stack.last, lastChar == dictionary[char]! else {
+                    return false
+                }
+                stack.removeLast()
+            }
+        }
+        return stack.count == 0
+    }
+}
+
