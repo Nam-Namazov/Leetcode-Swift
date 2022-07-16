6bce43a365d696f9368c0e4a6fe09482a53a4467
Namik <namazov306@gmail.com>
2022-07-16 03:45:04 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,18 @@
 import Foundation
+// MARK: - Palindrome Number
+
+class Solution {
+    func isPalindrome(_ x: Int) -> Bool {
+        guard x >= 0 else { return false }
+        if x < 10 { return true }
+        
+        var result = 0
+        var temp = x
+        
+        while temp != 0 {
+            result = (result * 10) + (temp % 10)
+            temp = temp / 10
+        }
+        return result == x
+    }
+}
