02885269f00aebb4a847dba5e4f7e67b488eb696
Namik <namazov306@gmail.com>
2022-07-16 04:01:43 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,21 @@
 import Foundation
+// MARK: - Fizz Buzz
+
+class Solution {
+    func fizzBuzz(_ n: Int) -> [String] {
+        var result: [String] = []
+        for i in 1...n {
+            
+            if i % 3 == 0 && i % 5 == 0 {
+                result.append("FizzBuzz")
+            } else if i % 3 == 0 {
+                result.append("Fizz")
+            } else if i % 5 == 0 {
+                result.append("Buzz")
+            } else {
+                result.append("\(i)")
+            }
+        }
+        return result
+    }
+}
