33e76d74a149961f79c83b4176b381a95634b2e9
Namik <namazov306@gmail.com>
2022-07-16 03:29:11 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,29 @@
 import Foundation
+// MARK: - Happy Number
+// 2 | 2 -> 4 -> 16 -> 37 -> 58 -> 89 -> 145 -> 42 -> 20 -> 4 - loop.
+// n = 2 -> false
+
+// 19 | 19 -> 82 -> 68 -> 100 -> 1
+// n = 19 -> true
+
+func isHappy(_ n: Int) -> Bool {
+    var set = Set<Int>()
+    var number = n
+    
+    while number != 1 {
+        guard !set.contains(number) else {
+            return false
+        }
+        set.insert(number)
+        
+        var temp = 0
+        while number != 0 {
+            temp += (number % 10) * (number % 10)
+            number = number / 10 // 1, 0.
+        }
+        number = temp
+    }
+    return true
+}
+
+print(isHappy(19))
