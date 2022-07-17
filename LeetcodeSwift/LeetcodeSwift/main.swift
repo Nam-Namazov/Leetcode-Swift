63b315d897ec887796e838e3063b90675cd68dbd
Namik <namazov306@gmail.com>
2022-07-18 02:04:28 +0300

    iteratively solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,32 @@
 import Foundation
+// MARK: - Reverse Linked List
+
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+// MARK: - iteratively
+// head -> 1 -> 2 -> 3 -> nil
+// nil <- 1 -> 2 -> 3 -> nil
+// nil <- 1 <- 2 -> 3 -> nil
+// nil <- 1 <- 2 <- 3 <- head
+// head -> 3 -> 2 -> 1 -> nil
+class Solution {
+    func reverseList(_ head: ListNode?) -> ListNode? {
+        var prev: ListNode? = nil
+        var nextHead: ListNode? = nil
+        var current = head
+        
+        while current != nil {
+            nextHead = current?.next
+            current?.next = prev
+            prev = current
+            current = nextHead
+        }
+        return prev
+    }
+}
