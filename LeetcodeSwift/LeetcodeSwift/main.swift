cb2007649f727e6b351ef9815ba2053f71a0bbeb
Namik <namazov306@gmail.com>
2022-07-19 17:11:46 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,22 @@
 import Foundation
+// MARK: - Sort Array By Parity 2
+
+class Solution {
+    func sortArrayByParityII(_ nums: [Int]) -> [Int] {
+        var array = nums
+        var odd = 0
+        var even = 1
+        
+        for i in nums {
+            if i % 2 == 0 {
+                array[odd] = i
+                odd += 2
+                
+            } else {
+                array[even] = i
+                even += 2
+            }
+        }
+        return array
+    }
+}
