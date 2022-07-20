fa030b0c89d7a08c620cc8bba4dcb38623ca126c
Namik <namazov306@gmail.com>
2022-07-20 18:43:49 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,40 @@
 import Foundation
+// MARK: - Isomorphic Strings
+
+class Solution {
+    func isIsomorphic(_ s: String, _ t: String) -> Bool {
+        var hashTable = [Character: String.Index]()
+        var hashTable2 = [Character: String.Index]()
+        
+        for i in s.indices {
+            guard hashTable[s[i]] == hashTable2[t[i]] else {
+                return false
+            }
+            hashTable[s[i]] = i
+            hashTable2[t[i]] = i
+        }
+        return true
+    }
+}
+
+
+ class Solution2 {
+     func isIsomorphic(_ s: String, _ t: String) -> Bool {
+         var hashTable: [Character: Character] = [:]
+         let arrayS = Array(s)
+         let arrayT = Array(t)
+        
+         for i in 0..<arrayS.count {
+             if let dict = hashTable[arrayS[i]] {
+                 if dict != arrayT[i] {
+                     return false
+                 }
+             } else if hashTable.values.contains(arrayT[i]) {
+                 return false
+             } else {
+                 hashTable[arrayS[i]] = arrayT[i]
+             }
+         }
+         return true
+     }
+ }
