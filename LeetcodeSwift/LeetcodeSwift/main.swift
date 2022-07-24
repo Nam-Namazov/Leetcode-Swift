8138bcd5803fdf3c63b4edd4e69d142d809038af
Namik <namazov306@gmail.com>
2022-07-25 00:56:01 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,24 @@
 import Foundation
+// MARK: - Find Pivot Index
+// [1, 7, 3, 6, 5, 6] -> nums[3]
+// Time complexity: O(n)
+// Space complexity: O(1)
+class Solution {
+    func pivotIndex(_ nums: [Int]) -> Int {
+        var allSum = 0
+        var leftSum = 0
+        
+        for num in nums {
+            allSum += num
+        }
+        
+        for i in 0..<nums.count {
+            if allSum - leftSum - nums[i] == leftSum {
+                return i
+            } else {
+                leftSum += nums[i]
+            }
+        }
+        return -1
+    }
+}
