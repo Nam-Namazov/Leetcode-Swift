efca28448278b977c0e1eb63bc1faa3ffa76033b
Namik <namazov306@gmail.com>
2022-07-26 12:24:39 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,28 @@
 import Foundation
+// MARK: - Middle of the Linked List
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+// Definition for singly-linked list
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init() { self.val = 0; self.next = nil; }
+    public init(_ val: Int) { self.val = val; self.next = nil; }
+    public init(_ val: Int, _ next: ListNode?) { self.val = val; self.next = next; }
+}
+
+// head = [1, 2, 3, 4, 5] -> the middle node = 3 -> output: [3, 4, 5]
+// head = [1, 2, 3, 4, 5, 6] -> the middle node = 4 -> output: [4, 5, 6]
+class Solution {
+    func middleNode(_ head: ListNode?) -> ListNode? {
+        var slow = head
+        var fast = head
+        
+        while fast != nil && fast?.next != nil {
+            slow = slow?.next
+            fast = fast?.next?.next
+        }
+        return slow
+    }
+}
