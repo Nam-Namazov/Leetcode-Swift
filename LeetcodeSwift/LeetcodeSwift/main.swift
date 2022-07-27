da104084baaf21880f1abe09da764f3510b3a817
Namik <namazov306@gmail.com>
2022-07-27 13:40:41 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,32 @@
 import Foundation
+// MARK: - Linked List Cycle
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+//Definition for singly-linked list.
+public class ListNode {
+    public var val: Int
+    public var next: ListNode?
+    public init(_ val: Int) {
+        self.val = val
+        self.next = nil
+    }
+}
+//              slow
+// 2 pointers: [1, 2, 3, 4], if fast === slow -> listnode have cycle -> return true
+//                    fast
+class Solution {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var slow = head
+        var fast = head
+        
+        while fast != nil && fast!.next != nil {
+            slow = slow!.next
+            fast = fast?.next?.next
+            
+            if slow === fast {
+                return true
+            }
+        }
+        return false
+    }
+}
