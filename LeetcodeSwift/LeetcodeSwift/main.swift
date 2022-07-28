a9f54d5d79cb8d3597f3b73c4546a85d4cd03fc9
Namik <namazov306@gmail.com>
2022-07-28 13:42:34 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,31 @@
 import Foundation
+// MARK: -  Longest Palindrome
+// Time Complexity: O(n)
+// Space Complexity: O(n)
+
+// "abbc" -> "bab" or "bcb" -> 3
+// "a" -> 1
+class Solution {
+    func longestPalindrome(_ s: String) -> Int {
+        var hashMap: [Character: Int] = [:]
+        var result = s.count
+        var oddValue = false
+        
+        guard result != 1 else { return 1 }
+        
+        for character in s {
+            hashMap[character, default: 0] += 1
+        }
+        
+        for (key, value) in hashMap {
+            if value % 2 == 1 {
+                if !oddValue {
+                    oddValue = true
+                } else {
+                    result -= 1
+                }
+            }
+        }
+        return result
+    }
+}
