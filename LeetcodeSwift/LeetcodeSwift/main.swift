ff389c5e04017f69a5b7886360a90af2e245009f
Namik <namazov306@gmail.com>
2022-07-30 13:15:14 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,22 @@
 import Foundation
+// MARK: - Binary Search
+// Time complexity: O(logn) ; Space Complexity: O(1)
+// nums = [-1,0,3,5,9,12], target = 9, return index of the target -> 4
+class Solution {
+    func search(_ nums: [Int], _ target: Int) -> Int {
+        var leftPointer = 0
+        var rightPointer = nums.count - 1
+        
+        while leftPointer <= rightPointer {
+            var midValue = leftPointer + (rightPointer - leftPointer) / 2
+            if nums[midValue] == target {
+                return midValue
+            } else if nums[midValue] > target {
+                rightPointer = midValue - 1
+            } else {
+                leftPointer = midValue + 1
+            }
+        }
+        return -1
+    }
+}
