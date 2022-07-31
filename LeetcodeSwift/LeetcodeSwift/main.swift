d16785c80161180d654839dcfbf202dfb4f14e5b
Namik <namazov306@gmail.com>
2022-07-31 20:31:42 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,39 @@
 import Foundation
+// MARK: - Validate Binary Search Tree
+// Time Complexity: O(n), Space Complexity: O(n)
+
+// Definition for a binary tree node.
+public class TreeNode {
+    public var val: Int
+    public var left: TreeNode?
+    public var right: TreeNode?
+    public init() { self.val = 0; self.left = nil; self.right = nil; }
+    public init(_ val: Int) { self.val = val; self.left = nil; self.right = nil; }
+    public init(_ val: Int, _ left: TreeNode?, _ right: TreeNode?) {
+        self.val = val
+        self.left = left
+        self.right = right
+    }
+}
+
+class Solution {
+    func isValidBST(_ root: TreeNode?) -> Bool {
+        return dfs(root, nil, nil)
+    }
+    
+    private func dfs(_ node: TreeNode?, _ min: Int?, _ max: Int?) -> Bool {
+        guard let node = node else {
+            return true
+        }
+        
+        if let min = min, node.val <= min {
+            return false
+        }
+        
+        if let max = max, node.val >= max {
+            return false
+        }
+        
+        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max)
+    }
+}
