7437fd88094baff3ab306059227fb8cf30bf3d75
Namik <namazov306@gmail.com>
2022-08-01 00:43:40 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,18 @@
 import Foundation
+// MARK: -  Build Array from Permutation
+
+class Solution {
+    func buildArray(_ nums: [Int]) -> [Int] {
+        guard nums.count != 1 else {
+            return nums
+        }
+        
+        var ans = [Int]()
+        
+        for i in 0..<nums.count {
+            ans.append(nums[nums[i]])
+        }
+        
+        return ans
+    }
+}
