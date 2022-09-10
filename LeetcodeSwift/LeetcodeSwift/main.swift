d60d9f202191d6d99cf069ee01f3e320f51c7b11
Namik <namazov306@gmail.com>
2022-09-10 22:32:27 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,24 @@
 import Foundation
+
+// MARK: - How Many Numbers Are Smaller Than the Current Number
+// O(n)
+// O(n^2)
+
+class Solution {
+    func smallerNumbersThanCurrent(_ nums: [Int]) -> [Int] {
+        var result: [Int] = []
+        
+        for _ in 0..<nums.count {
+            result.append(0)
+        }
+
+        for i in 0..<nums.count {
+            for j in 0..<nums.count {
+                if nums[i] > nums[j] {
+                    result[i] += 1
+                }
+            }
+        }
+        return result
+    }
+}
