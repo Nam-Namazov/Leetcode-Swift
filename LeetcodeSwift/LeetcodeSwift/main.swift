712cbc7a8bed21d913c79847198dcbc9515bb179
Namik <namazov306@gmail.com>
2022-09-13 19:31:01 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,19 @@
 import Foundation
+
+// MARK: - Length of Last Word
+
+// Time: O(n), Space: O(1)
+class Solution {
+    func lengthOfLastWord(_ s: String) -> Int {
+        var counter = 0
+        
+        for character in s.reversed() {
+            if character != " " {
+                counter += 1
+            } else if counter > 0 {
+                return counter
+            }
+        }
+        return counter
+    }
+}
