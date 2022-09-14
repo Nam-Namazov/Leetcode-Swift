ae4e4512ba0e11ad662393923a91a7e9b452f23c
Namik <namazov306@gmail.com>
2022-09-14 16:42:16 +0300

    bad solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,21 @@
 import Foundation
+
+// MARK: - Number of Arithmetic Triplets
+
+// O(n^3), O(1)
+class Solution {
+    func arithmeticTriplets(_ nums: [Int], _ diff: Int) -> Int {
+        var counter = 0
+        
+        for i in 0..<nums.count {
+            for j in 0..<nums.count {
+                for k in 0..<nums.count where i < j && j < k {
+                    if nums[j] - nums[i] == diff && nums[k] - nums[j] == diff {
+                        counter += 1
+                    }
+                }
+            }
+        }
+        return counter
+    }
+}
