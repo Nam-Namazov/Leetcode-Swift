d849a5a4ef6a54a0ad4198fb8f9d0dfc0976f3a4
Namik <namazov306@gmail.com>
2022-09-16 22:50:59 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,30 @@
 import Foundation
+
+// MARK: - Find All Lonely Numbers in the Array
+
+class Solution {
+    func findLonely(_ nums: [Int]) -> [Int] {
+        var hashMap: [Int: Int] = [:]
+        var result = [Int]()
+        
+        for num in nums {
+            if hashMap[num] != nil {
+                hashMap[num]! += 1
+            } else {
+                hashMap[num] = 1
+            }
+        }
+        
+        for (key, _) in hashMap {
+            if hashMap[key - 1] == nil &&
+                hashMap[key + 1] == nil &&
+                hashMap[key] == 1 {
+                result.append(key)
+            }
+        }
+        return result
+    }
+}
+
+
+
