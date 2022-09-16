84a8eaa802ea5c39e55d6b6a466cf2e38436ec79
Namik <namazov306@gmail.com>
2022-09-16 21:19:17 +0300

    solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,25 @@
 import Foundation
+
+// MARK: - Find All Duplicates in an Array
+
+class Solution {
+    func findDuplicates(_ nums: [Int]) -> [Int] {
+        var result: [Int] = []
+        var hashMap: [Int: Int] = [:]
+        
+        for num in nums {
+            if hashMap[num] != nil {
+                hashMap[num]! += 1
+            } else {
+                hashMap[num] = 1
+            }
+        }
+        
+        for (key, value) in hashMap {
+            if value == 2 {
+                result.append(key)
+            }
+        }
+        return result
+    }
+}
