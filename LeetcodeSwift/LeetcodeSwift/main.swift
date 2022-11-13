e4fd68cd2969739643e5c07e9ecc2815e7721780
Namik <namazov306@gmail.com>
2022-11-13 22:19:13 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,1 +1,25 @@
 import Foundation
+
+// MARK: -  Group Anagrams
+class Solution {
+    func groupAnagrams(_ strs: [String]) -> [[String]] {
+        var hashMap: [[Character]: [String]] = [:]
+        var result = [[String]]()
+        
+        for word in strs {
+            let wordSorted = word.sorted()
+            
+            if hashMap[wordSorted] == nil {
+                hashMap[wordSorted] = [word]
+            } else {
+                hashMap[wordSorted]?.append(word)
+            }
+        }
+        
+        for (_, value) in hashMap {
+            result.append(value)
+        }
+        
+        return result
+    }
+}
