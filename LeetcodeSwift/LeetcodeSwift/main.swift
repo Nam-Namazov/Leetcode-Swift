373827dda71d442a75316c296f02dff3a19b657d
Namik <namazov306@gmail.com>
2022-11-18 01:35:24 +0300

    Refactoring

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -3,28 +3,28 @@ import Foundation
 // MARK: - Longest Consecutive Sequence
 class Solution {
     func longestConsecutive(_ nums: [Int]) -> Int {
-
-        guard nums.count > 1 else {
+        
+        if nums.count == 0 || nums.count == 1 {
             return nums.count
         }
 
         let hashSet = Set(nums)
         var longestStreak = 0
-
+        
         for num in hashSet {
             if !hashSet.contains(num - 1) {
                 var currentNum = num
                 var currentStreak = 1
-
+                
                 while hashSet.contains(currentNum + 1) {
                     currentNum += 1
                     currentStreak += 1
                 }
-
+                
                 longestStreak = max(longestStreak, currentStreak)
             }
         }
-
+        
         return longestStreak
     }
 }
