eabc39d806a944ce2f267c7038f77538f03d523d
Namik <namazov306@gmail.com>
2022-11-26 22:05:17 +0300

    Solution two pointers

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,21 @@
 import Foundation
  
+// MARK: - Two Sum II - Input Array Is Sorted
+
+class Solution {
+    func twoSum(_ numbers: [Int], _ target: Int) -> [Int] {
+        var leftPointer = 0
+        var rightPointer = numbers.count - 1
+        
+        while numbers[leftPointer] + numbers[rightPointer] != target {
+            if numbers[leftPointer] + numbers[rightPointer] > target {
+                rightPointer -= 1
+            } else {
+                leftPointer += 1
+            }
+        }
+        
+        return [leftPointer + 1, rightPointer + 1]
+    }
+}
+
