e6c0e5be68aaafb57eeabd368c45f6dbd83187db
Namik <namazov306@gmail.com>
2022-11-26 21:00:22 +0300

    Solution two pointers

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,36 @@
 import Foundation
- 
+
+// MARK: - Valid Palindrome
+
+class Solution {
+    func isPalindrome(_ s: String) -> Bool {
+        if s.isEmpty {
+            return true
+        }
+        
+        let stringArray = Array(s)
+        var leftPointer = 0
+        var rightPointer = stringArray.count - 1
+        
+        while leftPointer < rightPointer {
+            if !stringArray[leftPointer].isLetter && !stringArray[leftPointer].isNumber {
+                leftPointer += 1
+                continue
+            }
+            
+            if !stringArray[rightPointer].isLetter && !stringArray[rightPointer].isNumber {
+                rightPointer -= 1
+                continue
+            }
+            
+            guard stringArray[leftPointer].lowercased() == stringArray[rightPointer].lowercased() else {
+                return false
+            }
+            
+            leftPointer += 1
+            rightPointer -= 1
+        }
+        
+        return true
+    }
+}
