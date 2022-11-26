c74fa1e0d868d3ca4102d9cde70ae472f1c0e722
Namik <namazov306@gmail.com>
2022-11-27 01:14:38 +0300

    Solution Two pointers

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,27 @@
 import Foundation
+
+// MARK: - Container With Most Water
  
+class Solution {
+    func maxArea(_ height: [Int]) -> Int {
+        var maxArea = 0
+        var leftPointer = 0
+        var rightPointer = height.count - 1
+        
+        while leftPointer < rightPointer {
+            
+            let newArea = (rightPointer - leftPointer) * min(height[leftPointer], height[rightPointer])
+            if newArea > maxArea {
+                maxArea = newArea
+            }
+            
+            if height[leftPointer] < height[rightPointer] {
+                leftPointer += 1
+            } else {
+                rightPointer -= 1
+            }
+        }
+        
+        return maxArea
+    }
+}
