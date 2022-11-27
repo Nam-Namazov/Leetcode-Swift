a5fc1e2c8e4f15acf44fde8123539057f284a083
Namik <namazov306@gmail.com>
2022-11-27 16:50:28 +0300

    Solution two pointers, minMax, rightMax

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,33 @@
 import Foundation
  
+// MARK: - Trapping Rain Water
+
+class Solution {
+    func trap(_ height: [Int]) -> Int {
+        var leftPointer = 0
+        var rightPointer = height.count - 1
+        var leftMax = 0
+        var rightMax = 0
+        var result = 0
+
+        while leftPointer < rightPointer {
+            if height[leftPointer] < height[rightPointer] {
+                if height[leftPointer] >= leftMax {
+                    leftMax = height[leftPointer]
+                } else {
+                    result += leftMax - height[leftPointer]
+                }
+                leftPointer += 1
+            } else {
+                if height[rightPointer] >= rightMax {
+                    rightMax = height[rightPointer]
+                } else {
+                    result += rightMax - height[rightPointer]
+                }
+                rightPointer -= 1
+            }
+        }
+        
+        return result
+    }
+}
