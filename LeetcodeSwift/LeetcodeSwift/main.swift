46f598c5572f06f11d722b00c67dfac1a47e5d70
Namik <namazov306@gmail.com>
2022-12-11 02:25:06 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,17 @@
 import Foundation
  
+// MARK: - Minimum Difference Between Highest and Lowest of K Scores
+class Solution {
+    func minimumDifference(_ nums: [Int], _ k: Int) -> Int {
+        let nums = nums.sorted()
+        let k = k - 1
+        var min = Int.max
+        
+        for i in 0..<nums.count - k {
+            if nums[i + k] - nums[i] < min {
+                min = nums[i + k] - nums[i]
+            }
+        }
+        return min
+    }
+}
