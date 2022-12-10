51bb7652325ecb6d4a1ff6c014c440895b03c9bb
Namik <namazov306@gmail.com>
2022-12-11 02:19:49 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,17 @@
 import Foundation
- 
+
+// MARK: - Substrings of Size Three with Distinct Characters
+
+class Solution {
+    func countGoodSubstrings(_ s: String) -> Int {
+        if s.count < 3 { return 0 }
+        let s = Array(s)
+        var count = 0
+        for i in 0..<s.count-2 {
+            if s[i] != s[i+1] && s[i] != s[i+2] && s[i+1] != s[i+2] {
+                count += 1
+            }
+        }
+        return count
+    }
+}
