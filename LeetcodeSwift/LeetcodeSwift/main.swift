342f32df6794106f9cecd27998e1e18b6e03ba2e
Namik <namazov306@gmail.com>
2022-12-11 15:01:42 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,20 @@
 import Foundation
  
+// MARK: - Find the K-Beauty of a Number
+
+class Solution {
+    func divisorSubstrings(_ num: Int, _ k: Int) -> Int {
+        var counter = 0
+        let arrayNum = String(num).map { String($0) }
+        
+        for i in 0...arrayNum.count - k {
+
+            if Int(arrayNum[i..<i + k].joined(separator: ""))! != 0,
+               num % Int(arrayNum[i..<i + k].joined(separator: ""))! == 0 {
+                
+                counter += 1
+            }
+        }
+        return counter
+    }
+}
