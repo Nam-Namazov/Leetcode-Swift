d6e7dd13b019c8fbe3728f8a16ef4e1d11a8af58
Namik <namazov306@gmail.com>
2022-12-11 13:49:43 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,25 @@
 import Foundation
  
+// MARK: - Minimum Recolors to get k consecutive black blocks
+
+class Solution {
+    func minimumRecolors(_ blocks: String, _ k: Int) -> Int {
+        var min = Int.max
+        let blocks = Array(blocks)
+
+        for i in 0...blocks.count - k {
+            var counter = 0
+
+            for j in 0..<k {
+                if blocks[i + j] == "W" {
+                    counter += 1
+                }
+            }
+
+            if counter < min {
+                min = counter
+            }
+        }
+        return min
+    }
+}
