c5c19fd002e2425886767f9db8116db081d8edff
Namik <namazov306@gmail.com>
2022-12-12 04:23:44 +0300

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,26 @@
 import Foundation
- 
+
+// MARK: - Squares of a Sorted Array
+class Solution {
+    func sortedSquares(_ nums: [Int]) -> [Int] {
+        var result = [Int]()
+        var left = 0
+        var right = nums.count - 1
+
+        while left <= right {
+            if abs(nums[left]) > abs(nums[right]) {
+                result.append(squareOf(nums[left]))
+                left += 1
+            } else {
+                result.append(squareOf(nums[right]))
+                right -= 1
+            }
+        }
+
+        return result.reversed()
+    }
+
+    private func squareOf(_ number: Int) -> Int {
+        number * number
+    }
+}
