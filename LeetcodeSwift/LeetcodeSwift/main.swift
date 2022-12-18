cf2c089752aeeec79dec3a6b6c1e0bab27a69119
Namik <namazov306@gmail.com>
2022-12-18 15:00:26 +0400

    Greedy solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,20 @@
 import Foundation
- 
+
+// MARK: - Jump Game
+
+class Solution {
+    func canJump(_ nums: [Int]) -> Bool {
+        var goal = nums.count - 1
+        var i = nums.count - 2
+        
+        while i >= 0 {
+            if i + nums[i] >= goal {
+                goal = i
+            }
+            i -= 1
+        }
+        
+        return goal == 0
+    }
+}
+
