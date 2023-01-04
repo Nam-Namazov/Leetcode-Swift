9eee335b8baa65c74861ba48324500ab9aa3e6ba
Namik <namazov306@gmail.com>
2023-01-04 17:10:54 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,26 @@
 import Foundation
  
+class Solution {
+    func minimumRounds(_ tasks: [Int]) -> Int {
+        var hashMap: [Int: Int] = [:]
+        var minimumRoundsCounter = 0
+
+        for i in 0..<tasks.count {
+            if hashMap[tasks[i]] == nil {
+                hashMap[tasks[i]] = 1
+            } else {
+                hashMap[tasks[i]]! += 1
+            }
+        }
+
+        for (_, value) in hashMap {
+            if value == 1 {
+                return -1
+            }
+            
+            minimumRoundsCounter += (value + 2) / 3
+        }
+        
+        return minimumRoundsCounter
+    }
+}
