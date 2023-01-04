a5b6ffb1d9461d4fd478acb7153e6bf568eac67b
Namik <namazov306@gmail.com>
2023-01-04 17:40:49 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,26 @@
 import Foundation
  
+class Solution {
+    func wordPattern(_ pattern: String, _ s: String) -> Bool {
+        
+        let s = s.split(separator: " ")
+        var hashMap: [String: String] = [:]
+        let pattern = Array(pattern)
+        
+        if pattern.count != s.count {
+            return false
+        }
+        
+        for i in 0..<pattern.count {
+            if hashMap[String(pattern[i])] == nil {
+                hashMap[String(pattern[i])] = String(s[i])
+            }
+            
+            if hashMap[String(pattern[i])] != String(s[i]) {
+                return false
+            }
+        }
+        
+        return Set(hashMap.keys).count == Set(hashMap.values).count
+    }
+}
