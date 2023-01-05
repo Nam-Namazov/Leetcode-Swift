43a74a6ee8e9bc9996cd1bbc24d5bf4742c9a83f
Namik <namazov306@gmail.com>
2023-01-05 17:03:14 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,25 @@
 import Foundation
  
+class Solution {
+    func findMinArrowShots(_ points: [[Int]]) -> Int {
+        
+        let points = points.sorted { $0[1] < $1[1] }
+        var minimumNumberOfArrows = 1
+        var xEnd = points[0][1]
+        
+        if points.count == 1 {
+            return 1
+        }
+        
+        for i in 0..<points.count {
+            let xStart = points[i][0]
+            
+            if xEnd < xStart {
+                minimumNumberOfArrows += 1
+                xEnd = points[i][1]
+            }
+        }
+        
+        return minimumNumberOfArrows
+    }
+}
