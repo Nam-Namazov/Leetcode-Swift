e8f69c8ed68dd9d4d319da6f75ede3d88956a468
Namik <namazov306@gmail.com>
2023-01-06 12:20:02 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,21 @@
 import Foundation
- 
+
+class Solution {
+    func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
+        
+        if costs.count == 1 && costs[0] <= coins {
+            return 1
+        }
+        
+        let costs = costs.sorted()
+        var counter = 0
+        var costsSum = 0
+        
+        for i in 0..<costs.count where costsSum + costs[i] <= coins {
+            counter += 1
+            costsSum = costsSum + costs[i]
+        }
+        
+        return counter
+    }
+}
