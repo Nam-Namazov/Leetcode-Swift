01c107ccdd3a3840c1bad16b8f4287acb6541af1
Namik <namazov306@gmail.com>
2023-01-06 15:40:32 +0400

    Refactoring

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -77,3 +77,20 @@ class Solution3 {
         return counter
     }
 }
+
+class Solution4 {
+    func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
+        let costs = costs.sorted()
+        var coins = coins
+        
+        for i in 0..<costs.count {
+            if costs[i] > coins {
+                return i
+            }
+            
+            coins -= costs[i]
+        }
+        
+        return costs.count
+    }
+}
