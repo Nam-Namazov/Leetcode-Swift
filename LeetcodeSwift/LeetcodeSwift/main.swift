bc20c50a9f7075b9992db74e008ed04534e6a54e
Namik <namazov306@gmail.com>
2023-01-06 15:05:14 +0400

    Add bucket sort solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,6 +1,36 @@
 import Foundation
 
-class Solution {
+// MARK: - Bucket Sort
+class Solution1 {
+    func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
+        
+        if costs.count == 1 && costs[0] <= coins { return 1 }
+        
+        var coins = coins
+        var result = 0
+        var buckets = [Int](repeating: 0, count: costs.max()! + 1)
+        
+        for cost in costs {
+            buckets[cost] += 1
+        }
+        
+        for i in 0..<buckets.count {
+            if coins < i {
+                break
+            }
+            
+            if buckets[i] > 0 {
+                result += min(buckets[i], coins / i)
+                coins -= min(coins, i * buckets[i])
+            }
+        }
+        
+        return result
+    }
+}
+
+// MARK: - Sort
+class Solution2 {
     func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
         
         if costs.count == 1 && costs[0] <= coins {
@@ -11,16 +41,20 @@ class Solution {
         var counter = 0
         var costsSum = 0
         
-        for i in 0..<costs.count where costsSum + costs[i] <= coins {
-            counter += 1
-            costsSum = costsSum + costs[i]
+        for i in 0..<costs.count {
+            if costsSum + costs[i] <= coins {
+                counter += 1
+                costsSum = costsSum + costs[i]
+            } else {
+                break
+            }
         }
         
         return counter
     }
 }
 
-class Solution2 {
+class Solution3 {
     func maxIceCream(_ costs: [Int], _ coins: Int) -> Int {
         
         if costs.count == 1 && costs[0] <= coins {
@@ -31,9 +65,13 @@ class Solution2 {
         var coins = coins
         var counter = 0
         
-        for i in 0..<costs.count where costs[i] <= coins {
-            coins -= costs[i]
-            counter += 1
+        for i in 0..<costs.count {
+            if costs[i] <= coins {
+                coins -= costs[i]
+                counter += 1
+            } else {
+                break
+            }
         }
         
         return counter
