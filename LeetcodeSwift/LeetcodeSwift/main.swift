d3cb2cd2d66f412654d2316df4b6f86ba8e239a2
Namik <namazov306@gmail.com>
2023-01-07 15:11:50 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,25 @@
 import Foundation
- 
+
+class Solution {
+    func canCompleteCircuit(_ gas: [Int], _ cost: [Int]) -> Int {
+        
+        if gas.reduce(0, +) < cost.reduce(0, +) { return -1 }
+
+        var total = 0
+        var result = 0
+        
+        for i in 0..<gas.count { 
+            total += gas[i] - cost[i]
+            if total < 0 {
+                total = 0
+                result = i + 1
+            }
+        }
+        
+        return result
+    }
+}
+
+let solution = Solution()
+print(solution.canCompleteCircuit([1,2,3,4,5], [3,4,5,1,2]))
+
