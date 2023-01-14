e8043c9aa4c4afa57cfbd65eadbd90ba4a785e48
Namik <namazov306@gmail.com>
2023-01-15 01:04:03 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,40 @@
 import Foundation
- 
+
+class Solution {
+    func isValid(_ s: String) -> Bool {
+        if s.count % 2 != 0 { return false }
+        
+        let stack = Stack()
+        
+        for bracket in s {
+            switch bracket {
+            case "(":
+                stack.push(")")
+            case "{":
+                stack.push("}")
+            case "[":
+                stack.push("]")
+            default:
+                if bracket != stack.popLast() { return false }
+            }
+        }
+        
+        return stack.isEmpty()
+    }
+}
+
+final class Stack {
+    private var elements: [Character] = []
+    
+    func push(_ x: Character) {
+        elements.append(x)
+    }
+    
+    func popLast() -> Character? {
+        elements.popLast()
+    }
+    
+    func isEmpty() -> Bool {
+        elements.isEmpty
+    }
+}
