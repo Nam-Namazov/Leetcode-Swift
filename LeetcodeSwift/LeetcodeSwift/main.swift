7b5368ef1f921453f7443f777f2e970662c39bf6
Namik <namazov306@gmail.com>
2023-01-15 02:40:44 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,46 @@
 import Foundation
  
+class Solution {
+    func calPoints(_ operations: [String]) -> Int {
+        var stack = Stack()
+        var scoresSum = 0
+        
+        for operation in operations {
+            switch operation {
+            case "+":
+                let sumPreviousTwoScores = stack.peek()! + stack.elements.dropLast().last!
+                scoresSum += sumPreviousTwoScores
+                stack.push(sumPreviousTwoScores)
+            case "C":
+                scoresSum -= stack.peek()!
+                stack.popLast()
+            case "D":
+                let doublePreviousScore = 2 * stack.peek()!
+                scoresSum += doublePreviousScore
+                stack.push(doublePreviousScore)
+            default:
+                let score = Int(operation)!
+                stack.push(score)
+                scoresSum += score
+            }
+        }
+        
+        return scoresSum
+    }
+}
+
+final class Stack {
+    var elements: [Int] = []
+    
+    func push(_ element: Int) {
+        elements.append(element)
+    }
+    
+    func popLast() -> Int? {
+        elements.popLast()
+    }
+    
+    func peek() -> Int? {
+        return elements.last
+    }
+}
