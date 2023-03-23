a4731223f8e2271067edd5892e1d74acb5b1a493
Namik <namazov306@gmail.com>
2023-03-23 19:39:11 +0400

    Solution

--- LeetcodeSwift/LeetcodeSwift/main.swift
+++ LeetcodeSwift/LeetcodeSwift/main.swift
@@ -1,2 +1,15 @@
 import Foundation
  
+class Solution {
+    func findDisappearedNumbers(_ nums: [Int]) -> [Int] {
+        var result = [Int]()
+        
+        for i in 1...nums.count {
+            if !nums.contains(i) {
+                result.append(i)
+            }
+        }
+        
+        return result
+    }
+}
