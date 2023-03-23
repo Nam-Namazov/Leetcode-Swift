9f39fbdb561fd5d913f01d3fa8f83ca87da9fbda
Namik <namazov306@gmail.com>
2023-03-24 01:01:08 +0400

    Init tests and solution file

---
+++ LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
@@ -0,0 +1,25 @@
+import XCTest
+@testable import LeetcodeSwift
+
+final class LeetcodeSwiftTests: XCTestCase {
+    
+    var sut: Solution!
+    
+    override func setUp() {
+        super.setUp()
+        sut = Solution()
+    }
+    
+    override func tearDown() {
+        sut = nil
+        super.tearDown()
+    }
+    
+    func testSolution_() {
+        // given
+        
+        // when
+        
+        // then
+    }
+}
