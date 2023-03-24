0256094368479540a3526c4c13609082f9b2cd46
Namik <namazov306@gmail.com>
2023-03-25 00:36:43 +0400

    Solution and test

--- LeetcodeSwift/LeetcodeSwift/Solution.swift
+++ LeetcodeSwift/LeetcodeSwift/Solution.swift
@@ -1,5 +1,24 @@
 import Foundation
 
 class Solution {
-
+    func shuffle(_ nums: [Int], _ n: Int) -> [Int] {
+        var result = [Int]()
+        var L = 0
+        var R = nums.count - n
+        var isShuffle = true
+        
+        while R < nums.count {
+            if isShuffle {
+                result.append(nums[L])
+                L += 1
+                isShuffle = false
+            } else {
+                result.append(nums[R])
+                R += 1
+                isShuffle = true
+            }
+        }
+        
+        return result
+    }
 }
--- LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
+++ LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
@@ -15,11 +15,32 @@ final class LeetcodeSwiftTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSolution_() {
+    func testSolution_Shuffle() {
+        
         // given
+        
+        let nums1 = [2, 5, 1, 3, 4, 7]
+        let nums2 = [1, 2, 3, 4, 4, 3, 2, 1]
+        let nums3 = [1, 1, 2, 2]
+        
+        let n1 = 3
+        let n2 = 4
+        let n3 = 2
         
         // when
         
+        let result1 = sut.shuffle(nums1, n1)
+        let result2 = sut.shuffle(nums2, n2)
+        let result3 = sut.shuffle(nums3, n3)
+        
         // then
+        
+        let expected1 = [2, 3, 5, 4, 1, 7]
+        let expected2 = [1, 4, 2, 3, 3, 2, 4, 1]
+        let expected3 = [1, 2, 1, 2]
+        
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
