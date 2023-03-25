f1c5afefeb2cd593286897a40a43153d5502c841
Namik <namazov306@gmail.com>
2023-03-25 19:16:51 +0400

    Solution and test

--- LeetcodeSwift/LeetcodeSwift/Solution.swift
+++ LeetcodeSwift/LeetcodeSwift/Solution.swift
@@ -1,5 +1,23 @@
 import Foundation
 
 class Solution {
-
+    func arrayStringsAreEqual(_ word1: [String], _ word2: [String]) -> Bool {
+        // MARK: -  One line solution: word1.joined() == word2.joined()
+        
+        
+        // MARK: - Second solution
+        
+        var concatenatedWord1 = ""
+        var concatenatedWord2 = ""
+        
+        for string in word1 {
+            concatenatedWord1.append(string)
+        }
+        
+        for string in word2 {
+            concatenatedWord2.append(string)
+        }
+        
+        return concatenatedWord1 == concatenatedWord2
+    }
 }
--- LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
+++ LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
@@ -15,11 +15,29 @@ final class LeetcodeSwiftTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSolution_() {
+    func testSolution_ArrayStringsAreEqual() {
         // given
+        let word1 = ["ab", "c"]
+        let word2 = ["a", "bc"]
+        
+        let word3 = ["a", "cb"]
+        let word4 = ["ab", "c"]
+        
+        let word5 = ["abc", "d", "defg"]
+        let word6 = ["abcddefg"]
         
         // when
+        let result1 = sut.arrayStringsAreEqual(word1, word2)
+        let result2 = sut.arrayStringsAreEqual(word3, word4)
+        let result3 = sut.arrayStringsAreEqual(word5, word6)
         
         // then
+        let expected1 = true
+        let expected2 = false
+        let expected3 = true
+        
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
