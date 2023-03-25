177bfac90cea3804fb8282073ff3066187cdce03
Namik <namazov306@gmail.com>
2023-03-25 20:32:37 +0400

    Counting sort solution and test

--- LeetcodeSwift/LeetcodeSwift/Solution.swift
+++ LeetcodeSwift/LeetcodeSwift/Solution.swift
@@ -1,5 +1,39 @@
 import Foundation
 
 class Solution {
-
+    func heightChecker(_ heights: [Int]) -> Int {
+        
+        if heights.count == 1 { return 0 }
+        
+        let expected = countingSort(heights)
+        var counter = 0
+        
+        for i in 0..<heights.count {
+            if heights[i] != expected[i] {
+                counter += 1
+            }
+        }
+        
+        return counter
+    }
+    
+    private func countingSort(_ array: [Int]) -> [Int] {
+        let maxElement = array.max()!
+        var countArray = Array(repeating: 0, count: maxElement + 1)
+        
+        for element in array {
+            countArray[element] += 1
+        }
+        
+        var sortedArray = [Int]()
+        var i = 0
+        
+        while i < countArray.count {
+            let count = countArray[i]
+            sortedArray.append(contentsOf: Array(repeating: i, count: count))
+            i += 1
+        }
+        
+        return sortedArray
+    }
 }
--- LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
+++ LeetcodeSwift/LeetcodeSwiftTests/LeetcodeSwiftTests.swift
@@ -17,9 +17,22 @@ final class LeetcodeSwiftTests: XCTestCase {
     
     func testSolution_() {
         // given
+        let heights1 = [1,1,4,2,1,3]
+        let heights2 = [5,1,2,3,4]
+        let heights3 = [1,2,3,4,5]
         
         // when
+        let result1 = sut.heightChecker(heights1)
+        let result2 = sut.heightChecker(heights2)
+        let result3 = sut.heightChecker(heights3)
         
         // then
+        let expected1 = 3
+        let expected2 = 5
+        let expected3 = 0
+        
+        XCTAssertEqual(result1, expected1)
+        XCTAssertEqual(result2, expected2)
+        XCTAssertEqual(result3, expected3)
     }
 }
